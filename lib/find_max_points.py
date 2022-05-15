f5b89f93617ab333e0ac56251a460ab309416275
Zsolt Ero <zsolt.ero@gmail.com>
2022-05-16 00:52:20 +0200

    max points work

--- lib/find_max_points.py
+++ lib/find_max_points.py
@@ -6,15 +6,26 @@ from pygeos import from_geojson
 
 
 def find_max_points(*, fixes: list, airspaces: dict):
-    coords = [[f['longitude'], f['latitude'], f['gpsAltitude']] for f in fixes]
+    coords = [[f['longitude'], f['latitude']] for f in fixes]
+    altitudes = [[f['gpsAltitude']] for f in fixes]
     points = pygeos.points(coords)
+    altitudes = np.array(altitudes)
 
     for name, airspace_data in airspaces.items():
-        print(name)
-        get_max_point_in_geojson(points=points, airspace=airspace_data)
+        max_alt = get_max_point_in_geojson(
+            points=points, altitudes=altitudes, airspace=airspace_data
+        )
+        print(name, max_alt)
 
 
-def get_max_point_in_geojson(*, points, airspace: dict):
+def get_max_point_in_geojson(*, points: np.array, altitudes: np.array, airspace: dict):
     polygon = from_geojson(json.dumps(airspace['geometry']))
     pygeos.prepare(polygon)
     res = pygeos.contains(polygon, points)
+
+    if not np.any(res):
+        return None
+
+    # selected_points = points[res]
+    selected_altitudes = altitudes[res]
+    return selected_altitudes.max()
